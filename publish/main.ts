2aafdf693014a7a438b557b35b7436890974c428
lhwdev <lhwdev6@outlook.com>
2021-12-16 00:52:41 +0900

    Fixed errors

--- publish/main.ts
+++ publish/main.ts
@@ -2,8 +2,8 @@ import {
   parse,
 } from "https://deno.land/std@0.117.0/encoding/yaml.ts";
 
-import { resolve, join } from "https://deno.land/std@0.63.0/path/mod.ts";
-import { copy } from "https://deno.land/std@0.95.0/fs/mod.ts";
+import { resolve } from "https://deno.land/std@0.63.0/path/mod.ts";
+import { copy, ensureDir } from "https://deno.land/std@0.95.0/fs/mod.ts";
 
 type Config = {
   noJekyll: boolean
@@ -11,14 +11,19 @@ type Config = {
 
 
 const rootPath = resolve(".");
-const repoPath = join(rootPath, "repo");
-const outputPath = join(rootPath, "output");
+const repoPath = rootPath;
+const outputPath = `${rootPath}/output`;
 
-const config = parse(await Deno.readTextFile("config.yaml")) as Config;
+await ensureDir(outputPath); // done in workflow publish.yaml
+
+
+const config = parse(await Deno.readTextFile(`${repoPath}/config.yaml`)) as Config;
 
 if(config.noJekyll) {
   await Deno.writeTextFile(`${outputPath}/.nojekyll`, "");
 }
 
+await Deno.writeTextFile(`${outputPath}/hello.html`, `🎉 This file is generated from Deno + Github Actions/publish.\nDeno version: ${Deno.version.deno}`);
+
 // Public
-await copy(`${repoPath}/public`, `${outputPath}/public`);
+await copy(`${repoPath}/public`, `${outputPath}`, { overwrite: true });
